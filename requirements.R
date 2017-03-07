15f00eaaa98838bdd4cb3070f791915f783a2c2a
Richard Cotton <richierocks@gmail.com>
2017-03-07 10:46:10 -0500

    try to store datasets in /usr/local/share/datasets

--- requirements.R
+++ requirements.R
@@ -1,4 +1,6 @@
+data_dir <- "/usr/local/share/datasets"
+dir.create(data_dir)
 download.file(
   "http://s3.amazonaws.com/assets.datacamp.com/production/course_2016/datasets/coffee_clean_corp.rds",
-  "/home/repl/coffee_clean_corp.rds"
+  file.path(data_dir, "coffee_clean_corp.rds")
 )
